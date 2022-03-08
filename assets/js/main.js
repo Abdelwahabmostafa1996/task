dc6079ac78b61f1d0064e3dfdb996784a59f17f7
Your Name <49571769+Abdelwahabmostafa1996@users.noreply.github.com>
2022-03-08 10:34:56 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+   //to open popup
+
+   function openForm() {
+
+       document.getElementById("myForm").style.display = "block";
+   }
+
+
+   //to close popup
+   function closeForm() {
+
+       document.getElementById("myForm").style.display = "none";
+   }
